5ac6fd8c96dcaf351857a81e5613a4fe6cea7c91
Kaustubh Garg <kaustubhgarg@Kaustubhs-MacBook-Air.local>
2020-02-16 19:52:54 +0530

    Atlas Added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
+mongoose.connect("mongodb+srv://admin-kaustubhgarg:3108fetcha%23@cluster0-aegjo.mongodb.net/todolistDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
